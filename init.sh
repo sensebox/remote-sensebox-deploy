ed3be422db67aeb99a12d551a70423a1ddcd3575
Felix Erdmann <hello@felixerdmann.com>
2021-05-31 15:38:33 +0100

    use twitch stream

--- init.sh
+++ init.sh
@@ -2,4 +2,6 @@ cp ./dockerfiles/Dockerfile-backend ./backend/Dockerfile
 cp ./dockerfiles/Dockerfile-frontend ./frontend/Dockerfile
 
 cp ./resources/store.js ./frontend/src/store.js
+cp ./resources/SketchDetails.js ./frontend/src/Components/Home/SketchDetails.js
+cp ./resources/Home.js ./frontend/src/Components/Home.js
 cp ./resources/frontend.env ./frontend/.env
